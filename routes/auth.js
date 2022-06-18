d37c9a567e4e0af7be2e1c8aa654543d51f8a438
Cristian Alarcon <cristian.alarcon.rodriguez@gmail.com>
2022-06-18 10:48:46 +0200

    signup done

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,19 @@ const router = require("express").Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
+router.get('/signup', (req, res, next) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', async (req, res, next) => {
+    const {name, hashedPassword} = req.body;
+    if (!name || !hashedPassword) {
+        const error = 'Mail or pass incorrect';
+        res.render ('auth/signup', {error});
+        return;
+    }
+});
+
 
 
 module.exports= router;
